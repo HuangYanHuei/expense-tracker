cb29b547dc47bd1f45808b2241a0721f5fdeb94e
gene.31985906@gmail.com <gene.31985906@gmail.com>
2022-06-22 17:49:59 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -2,15 +2,20 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
 
 const Record = require('./models/record')
 
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, 'useCreateIndex': true })
+
+
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
@@ -33,6 +38,22 @@ app.get('/', (req, res) => {
     .then(records => res.render('index', { records }))
     .catch(error => console.log(error))
 })
+
+app.get('/expense-tracker/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/expense-tracker', (req, res) => {
+  const userId = req.user._id
+  const categoryId = req.category._id
+  const { name, date, amount } = req.body
+  return Record.create({
+    name, date, amount, userId, categoryId
+  })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
 
 // 設定 port 3000
 app.listen(3000, () => {

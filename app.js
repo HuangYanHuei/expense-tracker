ce4c7940d1977e648b9972ef315dabd5e9a6f852
gene.31985906@gmail.com <gene.31985906@gmail.com>
2022-06-22 22:37:17 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -15,6 +16,12 @@ app.use(express.urlencoded({ extended: true }))
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))

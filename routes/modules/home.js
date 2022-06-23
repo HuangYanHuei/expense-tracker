521d30f432371558bb96bafc873d7b32af22b3f0
gene.31985906@gmail.com <gene.31985906@gmail.com>
2022-06-23 17:40:54 +0800

    feat:add index total money function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,16 +3,36 @@ const express = require('express')
 const router = express.Router()
 // 引用 Todo model
 const Record = require('../../models/record')
-// 定義首頁路由
+const Category = require('../../models/category')
+
 router.get('/', (req, res) => {
   const userId = req.user._id
-  const category = req.body.category
-  // const categoryId = req.category._id
-  Record.find({ userId })
-    .lean()
-    .sort({ _id: 'asc' }) // desc
-    .then(records => res.render('index', { records }))
-    .catch(error => console.error(error))
+  const category = req.query.value
+  if (!category || category === 'all') {
+    return Record.find({ userId })
+      .lean()
+      .sort({ _id: 'asc' })
+      .then(records => {
+        let sum = 0
+        records.map(value => sum += value.amount)
+        res.render('index', { records, sum })
+      })
+  } else {
+    Category.findOne({ name: category })
+      .lean()
+      .then((categorys) => {
+        const categoryId = categorys._id
+        return Record.find({ userId, categoryId })
+          .lean()
+          .sort({ _id: 'asc' })
+          .then((records) => {
+            let sum = 0
+            records.map(value => sum += value.amount)
+            res.render('index', { records, sum })
+          })
+      })
+      .catch(error => console.error(error))
+  }
 })
 // 匯出路由模組
 module.exports = router
